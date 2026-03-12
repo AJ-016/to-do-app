6395f96132c2912f6cc646455ce456b9d5f8219a
Alwin K J <alwinjaison8023@email.com>
2026-03-12 21:33:26 +0530

    Added delete button and enter key feature

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-const input = document.getElementById("taskInput"); const button = document.getElementById("addBtn"); const list = document.getElementById("taskList"); button.addEventListener("click", function(){ const taskText = input.value; const li = document.createElement("li"); li.textContent = taskText; list.appendChild(li); input.value = ""; });
+const input = document.getElementById("taskInput");
+const button = document.getElementById("addBtn");
+const list = document.getElementById("taskList");
+
+button.addEventListener("click", function(){
+
+    const taskText = input.value;
+
+    // prevent empty tasks
+    if(taskText === "") return;
+
+    const li = document.createElement("li");
+    li.textContent = taskText;
+
+    // create delete button
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "Delete";
+
+    deleteBtn.onclick = function(){
+        li.remove();
+    };
+
+    li.appendChild(deleteBtn);
+
+    list.appendChild(li);
+
+    input.value = "";
+
+});
+
+input.addEventListener("keypress", function(e){
+    if(e.key === "Enter"){
+        button.click();
+    }
+});
